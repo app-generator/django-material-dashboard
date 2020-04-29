d0d6a95eb7906584ff98fcd8c5a1ef89ec1c8644
Adi Chirilov <developer@rosoftware.ro>
2020-04-29 20:23:41 +0300

    Django Dashboard Material - Code Refactoring, Remove Heroku Scripts

--- app/views.py
+++ app/views.py
@@ -8,11 +8,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
+from django import template
 
 @login_required(login_url="/login/")
 def index(request):
-    context = {'segment' : 'index'}
-    return render(request, "index.html", context)
+    return render(request, "index.html")
 
 @login_required(login_url="/login/")
 def pages(request):
@@ -20,15 +20,17 @@ def pages(request):
     # All resource paths end in .html.
     # Pick out the html file name from the url. And load that template.
     try:
-
+        
         load_template = request.path.split('/')[-1]
+        html_template = loader.get_template( load_template )
+        return HttpResponse(html_template.render(context, request))
+        
+    except template.TemplateDoesNotExist:
 
-        context['segment'] = load_template
-
-        template = loader.get_template('pages/' + load_template)
-        return HttpResponse(template.render(context, request))
+        html_template = loader.get_template( 'error-404.html' )
+        return HttpResponse(html_template.render(context, request))
 
     except:
-
-        template = loader.get_template( 'pages/error-404.html' )
-        return HttpResponse(template.render(context, request))
+    
+        html_template = loader.get_template( 'error-500.html' )
+        return HttpResponse(html_template.render(context, request))
