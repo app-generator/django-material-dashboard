61bd4621e455c017473e22b70c663e24e6f0b6bc
Adi Chirilov <developer@rosoftware.ro>
2020-05-30 08:23:44 +0300

    Patch #6 - error-404.html not used in all contexts

--- app/urls.py
+++ app/urls.py
@@ -8,11 +8,13 @@ from django.urls import path, re_path
 from app import views
 
 urlpatterns = [
-    # Matches any html file - to be used for gentella
-    # Avoid using your .html in your resources.
-    # Or create a separate django app.
-    re_path(r'^.*\.html', views.pages, name='pages'),
 
     # The home page
     path('', views.index, name='home'),
+
+    # Matches any html file - to be used for gentella
+    # Avoid using your .html in your resources.
+    # Or create a separate django app.
+    re_path(r'^.*\.*', views.pages, name='pages'),
+
 ]
